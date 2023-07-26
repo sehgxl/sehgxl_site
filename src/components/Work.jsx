42ac46e5d76a8d6365ea3cd2b319ec420c334944
sehgxl <abhinavsehgal802@gmail.com>
2023-07-26 21:09:08 +0530

    Fix /work mobile responsiveness

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import { ProjectLinks } from "../ProjectLinks";
 
 const Work = () => {
   return (
-    <section className="min-h-screen font-mono bg-neutral-950 text-white  gap-10 px-24 py-20">
+    <section className="min-h-screen font-mono bg-neutral-950 text-white  gap-10 px-8 sm:px-24 py-20">
       <h1 className="text-5xl font-bold text-center">Work</h1>
       <section className="grid mt-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {ProjectLinks.map((project, idx) => {
